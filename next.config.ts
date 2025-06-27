e5a1c946592ecf7b22b49739827921fcf7e77058
Diego E <diego28e@gmail.com>
2025-06-27 18:24:28 -0500

    update next.config.ts to serve images as-is, with no optimization

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import { type NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // Add this images configuration
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
