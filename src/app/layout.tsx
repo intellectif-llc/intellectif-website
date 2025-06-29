72121d20de492b0c348366806e815800e6d7f8a7
Diego E <diego28e@gmail.com>
2025-06-29 13:50:51 -0500

    updated default url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : "https://intellectif.com";
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
